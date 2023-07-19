35b84e8dcdd3891d49a00a673e2dd722511f1950
Aarya Shree Chapagain <caaryashree@gmail.com>
2023-07-19 15:15:14 +0545

    refactor:pass userState from parent component

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,39 +1,10 @@
-import { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { api } from '../../utils';
-import { setUser } from '../../features/user';
-import { UserState } from '../../features/types';
+import { UserState } from '../../features/user/user';
 
-export default function Dashboard() {
-  const dispatch = useDispatch();
-
-  const [profile, setProfile] = useState<UserState>();
-
-  useEffect(() => {
-    async function getProfile() {
-      const profile = await api.get('/api/user');
-
-      const userInfo: UserState = {
-        id: profile.data.id,
-        firstName: profile.data.first_name,
-        lastName: profile.data.last_name,
-        email: profile.data.email,
-      };
-
-      setProfile(userInfo);
-      dispatch(setUser(userInfo));
-    }
-
-    getProfile();
-  }, [profile]);
-
+export default function Dashboard({ userInfo }: { userInfo?: UserState }) {
   return (
     <div>
       <h1>Dashboard</h1>
-      <p>
-        Welcome, User
-        {profile === undefined ? 'user' : profile['firstName']}
-      </p>
+      <p>Welcome, {!userInfo ? 'user' : userInfo?.firstName}</p>
     </div>
   );
 }
