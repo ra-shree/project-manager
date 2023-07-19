3f856ee2bf3ce45d042403a24b02b67dd180692f
Aarya Shree Chapagain <caaryashree@gmail.com>
2023-07-19 15:12:00 +0545

    chore:renamed GuestNavbar to Navbar

--- src/app.tsx
+++ src/app.tsx
@@ -1,22 +1,50 @@
+import { useState, useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Box } from '@chakra-ui/react';
-import { GuestNavbar, Footer, UserNavbar } from './components/layout';
+import { Navbar, Footer } from './components';
+import { Home } from './pages/Home';
 import { SignInForm } from './pages/SignIn';
 import { SignUpForm } from './pages/SignUp';
 import { Dashboard } from './pages/Dashboard';
 import { Projects } from './pages/Projects';
+import { Tasks } from './pages/Tasks';
+import { CreateUserForm, UserList } from './pages/admin';
+import { UserState } from './features';
+import { useSelector } from 'react-redux';
 
 export default function App() {
-  const authenticated = sessionStorage.getItem('user') === null ? false : true;
+  const [userInfo, setUserInfo] = useState<UserState>({
+    id: 0,
+    firstName: '',
+    lastName: '',
+    email: '',
+    role: '',
+  });
+
+  const user = useSelector((state: any) => state.user);
+
+  useEffect(() => {
+    setUserInfo(user);
+  }, [user]);
+
   return (
     <>
-      {authenticated ? <UserNavbar /> : <GuestNavbar />}
+      <Navbar userInfo={userInfo} />
+
       <Box>
         <Routes>
+          <Route path="/home" element={<Home />} />
           <Route path="/signin" element={<SignInForm />} />
           <Route path="/signup" element={<SignUpForm />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route
+            path="/dashboard"
+            element={<Dashboard userInfo={userInfo} />}
+          />
+
           <Route path="/projects" element={<Projects />} />
+          <Route path="/tasks" element={<Tasks />} />
+          <Route path="/admin/user/create" element={<CreateUserForm />} />
+          <Route path="/admin/user" element={<UserList />} />
         </Routes>
       </Box>
       <Footer />
