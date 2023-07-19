75f724ce5954455911f0ba036c7f10ea74a3335f
Aarya Shree Chapagain <caaryashree@gmail.com>
2023-07-19 15:11:33 +0545

    refactor:unified the User and Guest Navbar into one

---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,168 @@
+import {
+  Box,
+  Flex,
+  Avatar,
+  HStack,
+  IconButton,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  useDisclosure,
+  useColorModeValue,
+  useBreakpointValue,
+  Stack,
+  Text,
+} from '@chakra-ui/react';
+import { Link, useNavigate } from 'react-router-dom';
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+import { useDispatch } from 'react-redux';
+import { api } from '../../utils';
+import { UserState, resetUser } from '../../features/user/user';
+
+interface Links {
+  name: string;
+  path: string;
+}
+
+const HomeLinks: Links[] = [
+  { name: 'Home', path: '/home' },
+  { name: 'Pricing', path: '/pricing' },
+  { name: 'About', path: '/about' },
+  { name: 'Contact', path: '/contact' },
+];
+
+const AdminLinks: Links[] = [
+  { name: 'Users', path: '/admin/users' },
+  { name: 'Projects', path: '/admin/projects' },
+];
+
+const ManagerLinks: Links[] = [
+  { name: 'Users', path: '/users' },
+  { name: 'Projects', path: '/projects' },
+];
+
+const DeveloperLinks: Links[] = [
+  { name: 'Tasks', path: '/tasks' },
+  { name: 'Projects', path: '/projects' },
+];
+
+export default function Navbar({ userInfo }: { userInfo: UserState }) {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  let NavLinks: Links[] = [];
+  if (!userInfo) {
+    NavLinks = HomeLinks;
+  } else if (userInfo.role === 'admin') {
+    NavLinks = AdminLinks;
+  } else if (userInfo.role === 'manager') {
+    NavLinks = ManagerLinks;
+  } else if (userInfo.role === 'developer') {
+    NavLinks = DeveloperLinks;
+  } else {
+    NavLinks = HomeLinks;
+  }
+
+  async function onLogout() {
+    dispatch(resetUser());
+    navigate('/home');
+    await api.post('/logout');
+  }
+
+  return (
+    <>
+      <Box bg={useColorModeValue('white', 'gray.800')} px={4}>
+        <Flex
+          bg={useColorModeValue('white', 'gray.800')}
+          color={useColorModeValue('gray.600', 'white')}
+          minH={'60px'}
+          py={{ base: 2 }}
+          px={{ base: 4 }}
+          borderBottom={1}
+          borderStyle={'solid'}
+          borderColor={useColorModeValue('gray.200', 'gray.900')}
+          alignItems={'center'}
+          justifyContent={'space-between'}>
+          <IconButton
+            size={'md'}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={'Open Menu'}
+            display={{ md: 'none' }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <HStack spacing={8} alignItems={'center'}>
+            <Text
+              textAlign={useBreakpointValue({
+                base: 'center',
+                md: 'left',
+              })}
+              fontFamily={'heading'}
+              color={useColorModeValue('gray.800', 'white')}>
+              Logo
+            </Text>
+            <HStack
+              as={'nav'}
+              spacing={4}
+              display={{ base: 'none', md: 'flex' }}>
+              {NavLinks.map((link) => NavLink(link))}
+            </HStack>
+          </HStack>
+
+          {userInfo?.id != 0 ? UserOptions(userInfo, onLogout) : LoginOptions()}
+        </Flex>
+      </Box>
+    </>
+  );
+}
+
+const NavLink = (link: Links) => (
+  <Link
+    className="px-2 py-1 rounded-md hover:bg-gray-200 transition duration-500 ease-in-out"
+    to={link.path}>
+    {link.name}
+  </Link>
+);
+
+const UserOptions = (userInfo: UserState, onLogout: () => Promise<void>) => (
+  <Flex alignItems={'center'}>
+    <Menu>
+      <MenuButton
+        as={Button}
+        rounded={'full'}
+        variant={'link'}
+        cursor={'pointer'}
+        minW={0}>
+        <Avatar size={'sm'} name={!userInfo ? '' : userInfo['firstName']} />
+      </MenuButton>
+      <MenuList>
+        <MenuItem>Profile</MenuItem>
+        <MenuItem onClick={onLogout}>Sign Out</MenuItem>
+      </MenuList>
+    </Menu>
+  </Flex>
+);
+
+const LoginOptions = () => (
+  <Stack
+    flex={{ base: 1, md: 0 }}
+    justify={'flex-end'}
+    direction={'row'}
+    spacing={6}>
+    <Button
+      as={'a'}
+      display={{ base: 'none', md: 'inline-flex' }}
+      fontSize={'sm'}
+      fontWeight={600}
+      color={'white'}
+      bg={'pink.400'}
+      href={'/signin'}
+      _hover={{
+        bg: 'pink.300',
+      }}>
+      Sign In
+    </Button>
+  </Stack>
+);
--- src/components/layout/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as GuestNavbar } from './GuestNavbar';
-export { default as Footer } from './Footer';
-export { default as UserNavbar } from './UserNavbar';
